375b908132d7b67a4d0bdf4cc37720c25bcf25c7
NetCore3 <46130787+netcore3@users.noreply.github.com>
2019-10-21 01:46:20 -0700

    Add files via upload

---
+++ Chapter 10/2/BenchmarkingLib/BenchmarkingLib/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+using BenchmarkDotNet.Running;
+using LibToBenchmark;
+
+namespace BenchmarkingLib
+{
+	[MemoryDiagnoser]
+	[DisassemblyDiagnoser(printAsm: true, printSource: true, printIL: true)]
+	public class Program
+	{
+		static List<HistoricalValue> HisticalPrices;
+
+		static Program()
+		{
+			HistoricalPriceReader reader = new HistoricalPriceReader();
+			HisticalPrices = reader.GetHistoricalQuotes("MSFT").ToList();
+		}
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<Program>();
+			Console.ReadKey();
+		}
+
+		[Benchmark]
+		//public static void CalculateWithLinq()
+		public void CalculateWithLinq()
+		{
+			var linqres = SimpleMovingAverage.CalculateSMALinq(HisticalPrices, 14);
+			Debug.WriteLine(linqres.Count);
+		}
+
+		[Benchmark]
+		public void CalculateWithNonLinq()
+		{
+			var res = SimpleMovingAverage.CalculateSMA(HisticalPrices, 14);
+			Debug.WriteLine(res.Count);
+		}
+	}
+}
